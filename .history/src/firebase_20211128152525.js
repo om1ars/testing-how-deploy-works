eb53534e22922eeadd72bb1e9d89c60e7280f4b2
omar-slf <nurullayevumarali1@gmail.com>
2021-11-28 15:27:25 +0500

    first commit

---
+++ .history/src/firebase_20211128152525.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2H_ZsbwnJ_XqjoB3JzEwIc5CmGpZd0KU",
+  authDomain: "test-deploy-97653.firebaseapp.com",
+  projectId: "test-deploy-97653",
+  storageBucket: "test-deploy-97653.appspot.com",
+  messagingSenderId: "247405602973",
+  appId: "1:247405602973:web:515c05aa8e300bd56d4c74"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ .history/src/firebase_20211128152528.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyB2H_ZsbwnJ_XqjoB3JzEwIc5CmGpZd0KU",
+  authDomain: "test-deploy-97653.firebaseapp.com",
+  projectId: "test-deploy-97653",
+  storageBucket: "test-deploy-97653.appspot.com",
+  messagingSenderId: "247405602973",
+  appId: "1:247405602973:web:515c05aa8e300bd56d4c74"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
